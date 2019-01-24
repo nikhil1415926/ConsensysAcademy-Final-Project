26d63702c80f0205c4922fb9b36b423560b3cfb8
Nikhil <4531201+nikhil1415926@users.noreply.github.com>
2019-01-24 01:37:42 -0500

    React components and CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MarketplaceContract from '../build/contracts/Marketplace.json'
+import getWeb3 from './utils/getWeb3'
+
+import AdminComponent from './AdminComponent';
+import StoreOwnerContainer from './StoreOwnerContainer';
+import ShopperContainer from './ShopperContainer';
+
+import './css/oswald.css'
+import './css/open-sans.css'
+import './css/pure-min.css'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      accountType: '',
+      web3: null,
+      marketplaceInstance: null,
+      currentAccount: ''
+    }
+  }
+
+  componentWillMount() {
+    // Get network provider and web3 instance.
+    // See utils/getWeb3 for more info.
+
+    getWeb3.then(results => {
+      this.setState({
+        web3: results.web3
+      })
+
+      // Instantiate contract once web3 provided.
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+    const contract = require('truffle-contract')
+    const marketplace = contract(MarketplaceContract)
+    marketplace.setProvider(this.state.web3.currentProvider)
+
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      marketplace.deployed()
+        .then((instance) => this.setState({marketplaceInstance: instance, currentAccount: accounts[0]}))
+        .then(() => this.state.marketplaceInstance.getUserType.call({from: accounts[0]}))
+        .then((accountType) => this.setState({ accountType: accountType }))
+    })
+  }
+
   render() {
+    const accountType = this.state.accountType;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main style={{marginTop: '25px'}} className="container">
+          <div className="pure-g">
+            <div className="pure-u-1-1">
+              {accountType === '' && <p>Loading...</p>}
+              {accountType === 'admin' && <AdminComponent 
+                  web3={this.state.web3}
+                  currentAccount={this.state.currentAccount} 
+                  marketplace={this.state.marketplaceInstance}></AdminComponent>}
+              {accountType === 'storeOwner' && <StoreOwnerContainer 
+                  web3={this.state.web3} 
+                  currentAccount={this.state.currentAccount} 
+                  marketplace={this.state.marketplaceInstance}></StoreOwnerContainer>}
+              {accountType === 'shopper' && <ShopperContainer 
+                  web3={this.state.web3} 
+                  currentAccount={this.state.currentAccount} 
+                  marketplace={this.state.marketplaceInstance}></ShopperContainer>}
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default App
